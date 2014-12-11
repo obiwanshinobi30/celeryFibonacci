cbd074ba33c1e377fa6125462a4d52f84cc7b695
Josh Sager <wjsager@users.noreply.github.com>
2014-12-11 18:57:53 +0000

    renamed to fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('tasks', backend='amqp', broker='amqp://')
+app = Celery('fibonacci', backend='amqp', broker='amqp://')
 
 @app.task
 def fib(n):
@@ -8,6 +8,4 @@ def fib(n):
 
   for x in range(n-1):
     a, b = b, a + b;
-    return x;
-
-print(fib(11));
+    return a;
